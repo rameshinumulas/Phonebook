06cfde595aa140c09babb4f6a6f122fa8e85411d
rameshinumulas <inumularamesh@gmail.com>
2020-12-18 17:41:55 +0530

    Updates

---
+++ precache-manifest.6f4d5cbf59492e5cf0ba6256722402f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b89a2983dfc98c26f4f1706d2893952",
+    "url": "/Phonebook/index.html"
+  },
+  {
+    "revision": "c5d659fd70d93ad978ad",
+    "url": "/Phonebook/static/css/main.d2e137aa.chunk.css"
+  },
+  {
+    "revision": "f4e08126e4fabeb0b1c8",
+    "url": "/Phonebook/static/js/2.025e3f2d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/Phonebook/static/js/2.025e3f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d659fd70d93ad978ad",
+    "url": "/Phonebook/static/js/main.836693ec.chunk.js"
+  },
+  {
+    "revision": "e7121ebc0e338a38873f",
+    "url": "/Phonebook/static/js/runtime-main.e64f749f.js"
+  }
+]);
